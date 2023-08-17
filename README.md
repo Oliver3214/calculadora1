9c9c50058ac1791dc67c01b9f594223df96d1a50
Oliver3214 <alexander.oliverpes@gmail.com>
2023-08-16 21:06:51 -0600

    calculadora

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# calculadora1
+# calculadora
+Oliver Alexander Perez Silvestre carnet 7691-22-7225  ejercicio tarea de una calculadora
---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author oliver
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+         calculadora_form frm = new calculadora_form();
+                frm.show();
+    }
+}
